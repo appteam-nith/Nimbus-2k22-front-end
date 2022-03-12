92462b44ea2567f3b82578237e82649cbe89f20c
skdbsp123 <skdbsp123@gmail.com>
2022-03-12 22:18:47 +0530

    Initial commit

---
+++ app/src/main/java/com/nith/nimbus2k22/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.nith.nimbus2k22.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nith.nimbus2k22.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+    }
+}
