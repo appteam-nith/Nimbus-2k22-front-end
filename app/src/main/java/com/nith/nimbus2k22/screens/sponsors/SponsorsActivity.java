b754bf1da907e8fcdbd9b621ebbcad8186996c27
sohail2000 <msohail1379@gmail.com>
2022-03-20 13:32:58 +0530

    sponsor template

---
+++ app/src/main/java/com/nith/nimbus2k22/screens/sponsors/SponsorsActivity.java
@@ -0,0 +1,16 @@
+package com.nith.nimbus2k22.screens.sponsors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nith.nimbus2k22.R;
+
+public class SponsorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sponsors);
+    }
+}
