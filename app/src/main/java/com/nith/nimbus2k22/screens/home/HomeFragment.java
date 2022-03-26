1bb8ffbcbdf77af2931e65db3a6c502bd1b4b1ec
sohail2000 <msohail1379@gmail.com>
2022-03-27 05:19:36 +0530

    home dot indicator

--- app/src/main/java/com/nith/nimbus2k22/screens/home/HomeFragment.java
+++ app/src/main/java/com/nith/nimbus2k22/screens/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.nith.nimbus2k22.screens.home;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +11,8 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.nith.nimbus2k22.R;
 import com.nith.nimbus2k22.adapters.EventsAdapter;
@@ -36,6 +39,10 @@ public class HomeFragment extends Fragment {
     private EventsAdapter eventsAdapter;
     private SponsorsAdapter sponsorsAdapter;
 
+    private LinearLayout sliderDotsPanel;
+    private int dotsCount;
+    private ImageView[] dots;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,6 +50,7 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         homeImgSliderVP2 = view.findViewById(R.id.home_img_slider_VP2);
+        sliderDotsPanel = view.findViewById(R.id.slider_dots_panel);
 
         List<HomeSilderItem> silderItemList = new ArrayList<>();
         silderItemList.add(new HomeSilderItem(R.drawable.cyberverse_home));
@@ -51,7 +59,33 @@ public class HomeFragment extends Fragment {
         silderItemList.add(new HomeSilderItem(R.drawable.cyberverse_home));
         silderItemList.add(new HomeSilderItem(R.drawable.cyberverse_home));
 
-        homeImgSliderVP2.setAdapter(new HomeImgSliderAdapter(silderItemList, homeImgSliderVP2));
+        HomeImgSliderAdapter viewPagerAdapter = new HomeImgSliderAdapter(silderItemList, homeImgSliderVP2);
+        homeImgSliderVP2.setAdapter(viewPagerAdapter);
+        dotsCount = viewPagerAdapter.getItemCount();
+        dots = new ImageView[dotsCount];
+
+        for(int i=0; i< dotsCount; i++){
+            dots[i] = new ImageView(getActivity());
+            dots[i].setImageResource(R.drawable.dot);
+
+
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8,0,8,0);
+            sliderDotsPanel.addView(dots[i],params);
+        }
+
+        dots[0].setImageResource(R.drawable.black_dot);
+        homeImgSliderVP2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                for (int i=0;i<dotsCount;i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity(),R.drawable.dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getActivity(),R.drawable.black_dot));
+            }
+        });
 
         //Event
         homeEventRV=view.findViewById(R.id.home_event_RV);
