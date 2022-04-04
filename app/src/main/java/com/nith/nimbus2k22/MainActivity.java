e392296da1c699db836b70eee2078dbc77c76c0f
khyati <sainikhyati2001@gmail.com>
2022-04-04 05:38:31 +0530

    fix the declaration of bottom navigation item

--- app/src/main/java/com/nith/nimbus2k22/MainActivity.java
+++ app/src/main/java/com/nith/nimbus2k22/MainActivity.java
@@ -2,45 +2,26 @@ package com.nith.nimbus2k22;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-
-import com.cloudinary.android.MediaManager;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.nith.nimbus2k22.screens.account.ViewProfileFragment;
 import com.nith.nimbus2k22.screens.eventsAndWorkshops.AllEventsAndWorkshopsFragment;
 import com.nith.nimbus2k22.screens.home.HomeFragment;
-import com.nith.nimbus2k22.screens.home.MemeManiaFragment;
 import com.nith.nimbus2k22.screens.quiz.AllQuizzesFragment;
 import com.nith.nimbus2k22.screens.sigma.SigmaFragment;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
     private  int flag = 1;
-    private ImageView one;
-    private ImageView two;
-    private ImageView three;
-    private ImageView four;
-    private ImageView five;
-    private ImageView six;
-    private ImageView seven;
-    private ImageView eight;
-    private ImageView nine;
-    private ImageView ten;
-    private ImageView elven;
-    private ImageView twelve;
-    private ImageView thirteen;
-    private ImageView fourteen;
-    private ImageView fifteen;
+    private BottomNavigationItemView one;
+    private BottomNavigationItemView two;
+    private BottomNavigationItemView three;
+    private BottomNavigationItemView four;
+    private BottomNavigationItemView five;
 
 
     @Override
@@ -75,37 +56,15 @@ public class MainActivity extends AppCompatActivity {
                 three.setVisibility(View.VISIBLE);
                 four.setVisibility(View.VISIBLE);
                 five.setVisibility(View.VISIBLE);
-                six.setVisibility(View.VISIBLE);
-                elven.setVisibility(View.VISIBLE);
-                seven.setVisibility(View.INVISIBLE);
-                eight.setVisibility(View.INVISIBLE);
-                nine.setVisibility(View.INVISIBLE);
-                ten.setVisibility(View.INVISIBLE);
-                twelve.setVisibility(View.INVISIBLE);
-                thirteen.setVisibility(View.INVISIBLE);
-                fourteen.setVisibility(View.INVISIBLE);
-                fifteen.setVisibility(View.INVISIBLE);
-
 
                 break;
             case 2:
                 replaceFragment(new AllQuizzesFragment());
-
                 two.setVisibility(View.INVISIBLE);
                 one.setVisibility(View.VISIBLE);
                 three.setVisibility(View.VISIBLE);
                 four.setVisibility(View.VISIBLE);
                 five.setVisibility(View.VISIBLE);
-                seven.setVisibility(View.VISIBLE);
-                twelve.setVisibility(View.VISIBLE);
-                six.setVisibility(View.INVISIBLE);
-                eight.setVisibility(View.INVISIBLE);
-                nine.setVisibility(View.INVISIBLE);
-                ten.setVisibility(View.INVISIBLE);
-                elven.setVisibility(View.INVISIBLE);
-                thirteen.setVisibility(View.INVISIBLE);
-                fourteen.setVisibility(View.INVISIBLE);
-                fifteen.setVisibility(View.INVISIBLE);
 
                 break;
             case 3:
@@ -115,16 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 one.setVisibility(View.VISIBLE);
                 four.setVisibility(View.VISIBLE);
                 five.setVisibility(View.VISIBLE);
-                eight.setVisibility(View.VISIBLE);
-                thirteen.setVisibility(View.VISIBLE);
-                seven.setVisibility(View.INVISIBLE);
-                six.setVisibility(View.INVISIBLE);
-                nine.setVisibility(View.INVISIBLE);
-                ten.setVisibility(View.INVISIBLE);
-                twelve.setVisibility(View.INVISIBLE);
-                elven.setVisibility(View.INVISIBLE);
-                fourteen.setVisibility(View.INVISIBLE);
-                fifteen.setVisibility(View.INVISIBLE);
 
                 break;
             case 4:
@@ -134,16 +83,6 @@ public class MainActivity extends AppCompatActivity {
                 three.setVisibility(View.VISIBLE);
                 one.setVisibility(View.VISIBLE);
                 five.setVisibility(View.VISIBLE);
-                nine.setVisibility(View.VISIBLE);
-                fourteen.setVisibility(View.VISIBLE);
-                seven.setVisibility(View.INVISIBLE);
-                eight.setVisibility(View.INVISIBLE);
-                six.setVisibility(View.INVISIBLE);
-                ten.setVisibility(View.INVISIBLE);
-                twelve.setVisibility(View.INVISIBLE);
-                thirteen.setVisibility(View.INVISIBLE);
-                elven.setVisibility(View.INVISIBLE);
-                fifteen.setVisibility(View.INVISIBLE);
 
                 break;
             case 5:
@@ -153,16 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 three.setVisibility(View.VISIBLE);
                 four.setVisibility(View.VISIBLE);
                 one.setVisibility(View.VISIBLE);
-                ten.setVisibility(View.VISIBLE);
-                fifteen.setVisibility(View.VISIBLE);
-                seven.setVisibility(View.INVISIBLE);
-                eight.setVisibility(View.INVISIBLE);
-                nine.setVisibility(View.INVISIBLE);
-                six.setVisibility(View.INVISIBLE);
-                twelve.setVisibility(View.INVISIBLE);
-                thirteen.setVisibility(View.INVISIBLE);
-                fourteen.setVisibility(View.INVISIBLE);
-                elven.setVisibility(View.INVISIBLE);
 
                 break;
         }
