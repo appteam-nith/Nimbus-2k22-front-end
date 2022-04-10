ac43501f4ea68caa06160dd1d349c71de1144040
1someshverma <1someshverma@gmail.com>
2022-04-10 23:25:44 +0530

    teamlist

--- app/src/main/java/com/nith/nimbus2k22/adapters/TeamListAdapter.java
+++ app/src/main/java/com/nith/nimbus2k22/adapters/TeamListAdapter.java
@@ -1,40 +1,54 @@
 package com.nith.nimbus2k22.adapters;
 
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nith.nimbus2k22.R;
 import com.nith.nimbus2k22.modals.TeamListModal;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TeamListAdapter extends RecyclerView.Adapter<TeamListAdapter.MyViewHolder> {
-    private final List<TeamListModal>teamListModals=new ArrayList<>();
-    
+public class TeamListAdapter extends RecyclerView.Adapter<TeamListAdapter.DepViewHolder> {
+    private List<TeamListModal> list = new ArrayList<>();
+    private Context context;
 
     @NonNull
     @Override
-    public TeamListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public DepViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.id.,parent,false);
+        return new DepViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TeamListAdapter.MyViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull DepViewHolder holder, int position) {
+        TeamListModal teams=list.get(position);
+        holder.teamImage.setImageURI(Uri.parse(teams.getImage()));
+        holder.teamName.setText(teams.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-
-        public MyViewHolder(@NonNull View itemView) {
+    public class DepViewHolder extends RecyclerView.ViewHolder {
+        ImageView teamImage;
+        TextView teamName;
+        public DepViewHolder(@NonNull View itemView) {
             super(itemView);
+            teamImage=itemView.findViewById(R.id.teamImg);
+            teamName=itemView.findViewById(R.id.teamName);
         }
     }
+
+
 }
